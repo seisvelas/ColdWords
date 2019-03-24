e41749351a7eccb6dc871c42ebcebe514e5f9868
donobanmunoz <dmunoztejeda@gmail.com>
2019-03-24 03:21:36 -0600

    added visualization generator

--- analyze_vocab.rkt
+++ analyze_vocab.rkt
@@ -2,6 +2,7 @@
 
 (require db)
 (require db/util/postgresql)
+(require plot)
 
 ; load credentials from .py
 (define credentials (string-split (file->string "database_credentials.py") "\""))
@@ -32,14 +33,31 @@
 
 (define (analyze-wordset wordset)
   (let ((ideology (vector-ref wordset 0))
-        (words (take (pg-array->list (vector-ref wordset 1)) WORDS-LIMIT)))
+        (words (take (pg-array->list (vector-ref wordset 1))
+                     WORDS-LIMIT)))
     (list ideology
           (exact->inexact
+           ; TTR formula: length(set(text)) / length(text)
            (/ (length (remove-duplicates words))
               (length words))))))
 
-; returns big lists of all words found for each ideology 
-(let* ((ideologies-words (query-rows pgc WORD-ARRAYS-QUERY)))
-  (display (map analyze-wordset
-                ideologies-words)))
+(define (ideologies->histogram ideologies)
+  (plot-file
+ (discrete-histogram
+  (map (lambda (x)
+         (vector (first x)
+                 (ivl .26 (second x))))
+       ideologies)
+  #:color "darkred"
+  #:line-color "black"
+  #:y-min .275
+  #:y-max .283)
+ #:title "Ideologies"
+ #:x-label ""
+ #:y-label "Type Token Ratio"
+ "histogram.png"))
 
+; returns big lists of all words found for each ideology 
+(let ((ideologies-words (query-rows pgc WORD-ARRAYS-QUERY)))
+  (ideologies->histogram (map analyze-wordset
+                              ideologies-words)))
